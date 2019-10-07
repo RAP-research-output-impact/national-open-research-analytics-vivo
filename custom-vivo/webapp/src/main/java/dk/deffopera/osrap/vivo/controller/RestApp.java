6ff24cbaee855bdecad97966cadcf4a9e46f56ff
Brian Lowe <brian@ontocale.com>
2019-10-07 14:30:15 +0200

    Custom VIVO structure based on VIVO 1.10

---
+++ custom-vivo/webapp/src/main/java/dk/deffopera/osrap/vivo/controller/RestApp.java
@@ -0,0 +1,8 @@
+package dk.deffopera.osrap.vivo.controller;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/vds")
+public class RestApp extends ResourceConfig {}

94170266fa0bf146a9af93afae563f08a81e947f
Brian Lowe <brian@ontocale.com>
2019-10-09 11:38:35 +0200

    Add Dimensions connector

--- etl/src/main/java/dk/deffopera/osrap/vivo/etl/OsrapEtl.java
+++ etl/src/main/java/dk/deffopera/osrap/vivo/etl/OsrapEtl.java
@@ -3,181 +3,138 @@ package dk.deffopera.osrap.vivo.etl;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
-import org.apache.jena.util.ResourceUtils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import dk.deffopera.osrap.vivo.etl.util.HttpUtils;
-import dk.deffopera.osrap.vivo.etl.util.JsonToXMLConverter;
-import dk.deffopera.osrap.vivo.etl.util.RdfUtils;
-import dk.deffopera.osrap.vivo.etl.util.XmlToRdf;
+import dk.deffopera.osrap.vivo.etl.datasource.DataSource;
+import dk.deffopera.osrap.vivo.etl.datasource.connector.dimensions.DimensionsConnector;
+import dk.deffopera.osrap.vivo.etl.util.SparqlEndpointParams;
 
 public class OsrapEtl {
 
-    private static final String DIMENSIONS_API = "https://app.dimensions.ai/api/";
-    private static final String ABOX = "http://vivo.deffopera.dk/individual/";
-    private static final long REQUEST_INTERVAL = 2000; // ms
-    
     private static final Log log = LogFactory.getLog(OsrapEtl.class);
-    private HttpUtils httpUtils = new HttpUtils();
     
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         if(args.length < 3) {
-            System.out.println("Usage: <dimensions username> <dimensions password> <output file path>");
+            System.out.println("Usage: " 
+                    + "dimensions outputfile dimensionsUsername=<username> dimensionsPassword=<password> " 
+                    + "[dataDir=] [endpointURI= endpointUpdateURI= username= password=] [authUsername= authPassword=] [graphURI=] "
+                    + "[limit]");
             return;
         }
-        OsrapEtl etl = new OsrapEtl();
-        String token = etl.getToken(args[0], args[1]);
-        String filepath = args[2];
-        Model rdf = ModelFactory.createDefaultModel();        
-        rdf.add(etl.toRdf(etl.getPubs(token)));        
-        rdf.add(etl.toRdf(etl.getGrants(token)));
-        rdf.add(etl.toRdf(etl.getPatents(token)));
-        rdf.add(etl.toRdf(etl.getClinicalTrials(token)));
-        OutputStream out;
-        try {
-            out = new FileOutputStream(new File(filepath));
-            rdf.write(out, "TTL");
-            log.info("Wrote " + rdf.size() + " triples to " + filepath);
-        } catch (FileNotFoundException e) {
-            log.error(e, e);
-            throw new RuntimeException(e);
+        List<String> queryTerms = new LinkedList<String>(
+                Arrays.asList(args));
+        String connectorName  = queryTerms.remove(0);
+        String outputFileName = queryTerms.remove(0);
+        int limit = getLimit(queryTerms);
+        if(limit < Integer.MAX_VALUE) {
+            log.info("Retrieving a limit of " + limit + " records");
+        }
+        SparqlEndpointParams endpointParameters = getEndpointParams(queryTerms);
+        DataSource connector = getConnector(connectorName, queryTerms);
+        connector.getConfiguration().getParameterMap().put("dataDir", getDataDir(queryTerms));
+        //connector.getConfiguration().setQueryTerms(queryTerms);
+        connector.getConfiguration().setEndpointParameters(endpointParameters);
+        connector.getConfiguration().setLimit(limit);
+        connector.getConfiguration().setResultsGraphURI(getParameter(queryTerms, "graphURI"));
+        connector.run();
+        Model result = connector.getResult();
+        if(result == null) {
+            log.warn("result is null");
+        } else {
+            File outputFile = new File(outputFileName);
+            FileOutputStream fos;
+            try {
+                log.info("Writing output to " + outputFile);
+                fos = new FileOutputStream(outputFile);
+                result.write(fos, "N3");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
+    private static DataSource getConnector(String connectorName, List<String> queryTerms) {
+        DataSource connector = null;
+        if ("dimensions".equals(connectorName)) {
+            connector = new DimensionsConnector(
+                    getParameter(queryTerms, "dimensionsUsername"), 
+                    getParameter(queryTerms, "dimensionsPassword"));
+        } else {
+            throw new RuntimeException("Connector not found: " 
+                    + connectorName);
+        }        
+        return connector;
+    }
+    
+    private static int getLimit(List<String> queryTerms) {
+        if(!queryTerms.isEmpty()) {
+            String possibleLimit = queryTerms.get(queryTerms.size() - 1);            
+            try {
+                int limit = Integer.parseInt(possibleLimit, 10);
+                queryTerms.remove(queryTerms.size() - 1);
+                return limit;
+            } catch (NumberFormatException e) {
+                // no limit argument present; move on
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+    
+    private static String getDataDir(List<String> queryTerms) {
+        if(!queryTerms.isEmpty() && queryTerms.get(0).startsWith("dataDir=")) {
+            return queryTerms.get(0).substring("dataDir=".length());            
+        } else {
+            return null;
+        }
+    }
+    
+    private static SparqlEndpointParams getEndpointParams(List<String> queryTerms) {
+        SparqlEndpointParams params = new SparqlEndpointParams();
+        int toRemove = 0;
+        for (String queryTerm : queryTerms) {
+            if(queryTerm.startsWith("endpointURI=")) {
+                params.setEndpointURI(queryTerm.substring("endpointURI=".length()));
+                toRemove++;
+            } else if (queryTerm.startsWith("endpointUpdateURI=")) {
+                params.setEndpointUpdateURI(queryTerm.substring("endpointUpdateURI=".length()));
+                toRemove++;
+            } else if (queryTerm.startsWith("username=")) {
+                params.setUsername(queryTerm.substring("username=".length()));
+                toRemove++;
+            } else if (queryTerm.startsWith("password=")) {
+                params.setPassword(queryTerm.substring("password=".length()));
+                toRemove++;
+            } else if (queryTerm.startsWith("authUsername=")) {
+                params.setHttpBasicAuthUsername(queryTerm.substring("authUsername=".length()));
+                toRemove++;
+            } else if (queryTerm.startsWith("authPassword=")) {
+                params.setHttpBasicAuthPassword(queryTerm.substring("authPassword=".length()));
+                toRemove++;
+            } 
+        }
+        for(int i = 0; i < toRemove; i++) {
+            queryTerms.remove(0);
+        }
+        if(toRemove > 0) {
+            return params;    
+        } else {
+            return null;
         }        
     }
     
-    long lastRequest = 0;
-    
-    private String getDslResponse(String dslQuery, String token) 
-            throws InterruptedException {
-        long now = System.currentTimeMillis();
-        long toWait = REQUEST_INTERVAL - (now - lastRequest);        
-        if(toWait > 0) {
-            Thread.sleep(toWait);
-        }
-        lastRequest = System.currentTimeMillis();
-        return httpUtils.getHttpPostResponse(DIMENSIONS_API + "dsl.json",
-                dslQuery, "application/json", token);        
-    }
-    
-    private Model toRdf(String data) {                
-        try {
-            //ObjectMapper mapper = new ObjectMapper();
-            //JsonNode dataObj;
-            //dataObj = mapper.readTree(data);
-            //System.out.println(mapper.writerWithDefaultPrettyPrinter()
-            //        .writeValueAsString(dataObj));
-            JsonToXMLConverter json2xml = new JsonToXMLConverter();
-            XmlToRdf xml2rdf = new XmlToRdf();
-            RdfUtils rdfUtils = new RdfUtils();
-            String xml = json2xml.convertJsonToXml(data);
-            Model rdf = xml2rdf.toRDF(xml);
-            rdf = rdfUtils.renameBNodes(rdf, ABOX + "n", rdf);
-            rdf = renameByIdentifier(rdf, rdf.getProperty(
-                    XmlToRdf.GENERIC_NS + "id"), ABOX, "");
-            return rdf;
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-    private String getToken(String username, String password) {
-        ObjectNode json = JsonNodeFactory.instance.objectNode();
-        json.put("username", username);
-        json.put("password", password);
-        String tokenJson = httpUtils.getHttpPostResponse(
-                DIMENSIONS_API + "auth.json", json.toString(), "application/json");
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode tokenObj;
-        try {
-            tokenObj = mapper.readTree(tokenJson);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return tokenObj.get("token").textValue();
-    }
-    
-    private String getGrants(String token) throws InterruptedException {
-        String queryStr = "search grants where"
-                + " (start_year >= 2014 and start_year <= 2017 and research_orgs.id = \"grid.5170.3\")"
-                + " return grants[active_year + end_date + funders + id + project_num + start_date + start_year + title + abstract + funding_eur + grant_number + investigator_details + research_orgs]"
-                + " limit 100";
-        return getDslResponse(queryStr, token);
-    }
-    
-    private String getPubs(String token) throws InterruptedException {
-        String queryStr = "search publications where"
-                + " (year >= 2014 and year <= 2017 and research_orgs.id = \"grid.5170.3\" and type in [\"article\", \"chapter\", \"proceeding\"])"
-                //+ " return publications"
-                + " return publications[id + type + title + authors + doi + pmid + pmcid + date + year + mesh_terms + journal + issn + volume + issue]"
-                + " limit 100";
-        return getDslResponse(queryStr, token);
-    }
-    
-    private String getPatents(String token) throws InterruptedException {
-        String queryStr = "search patents where"
-                + " (granted_year >= 2014 and granted_year <= 2017 and assignees.id = \"grid.5170.3\")"
-                + " return patents[assignees + granted_year + id + title + inventor_names + funders + abstract + associated_grant_ids]"
-                + " limit 100";
-        return getDslResponse(queryStr, token);
-    }
-    
-    private String getClinicalTrials(String token) throws InterruptedException {
-        String queryStr = "search clinical_trials where"
-                + " (active_years >= 2014 and active_years <= 2017 and organizations.id = \"grid.5170.3\")"
-                + " return clinical_trials"
-                + " limit 100";
-        return getDslResponse(queryStr, token);
-    }
-    
-    protected static Model renameByIdentifier(Model m, Property identifier, 
-            String namespace, String localNamePrefix) {
-        Map<Resource, String> idMap = new HashMap<Resource, String>();
-        StmtIterator sit = m.listStatements(null, identifier, (RDFNode) null);
-        while(sit.hasNext()) {
-            Statement stmt = sit.next();
-            if(stmt.getObject().isLiteral()) {
-                idMap.put(stmt.getSubject(), 
-                        //stripNonWordChars(stmt.getObject().asLiteral().getLexicalForm()));
-                        stmt.getObject().asLiteral().getLexicalForm());
-            }
-        }
-        for(Resource res : idMap.keySet()) {
-            ResourceUtils.renameResource(
-                    res, namespace + localNamePrefix + idMap.get(res));
-        }
-        return m;
-    }
-    
-    protected static String stripNonWordChars(String value) {
-        if(value == null) {
-            return value;
+    private static String getParameter(List<String> queryTerms, String paramName) {
+        if(!queryTerms.isEmpty() && queryTerms.get(0).startsWith(paramName + "=")) {
+            String graphParam = queryTerms.remove(0);
+            return graphParam.substring((paramName + "=").length());
         } else {
-            return value.replaceAll("\\W", "");
+            return null;
         }
     }
     
 }
-
-
---
+++ etl/src/main/java/dk/deffopera/osrap/vivo/etl/datasource/connector/dimensions/DimensionsConnector.java
@@ -0,0 +1,311 @@
+package dk.deffopera.osrap.vivo.etl.datasource.connector.dimensions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.util.ResourceUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import dk.deffopera.osrap.vivo.etl.datasource.DataSource;
+import dk.deffopera.osrap.vivo.etl.datasource.IteratorWithSize;
+import dk.deffopera.osrap.vivo.etl.datasource.connector.ConnectorDataSource;
+import dk.deffopera.osrap.vivo.etl.util.HttpUtils;
+import dk.deffopera.osrap.vivo.etl.util.JsonToXMLConverter;
+import dk.deffopera.osrap.vivo.etl.util.RdfUtils;
+import dk.deffopera.osrap.vivo.etl.util.XmlToRdf;
+
+public class DimensionsConnector extends ConnectorDataSource 
+        implements DataSource {
+
+    private static final String DIMENSIONS_API = "https://app.dimensions.ai/api/";
+    private static final String ABOX = "http://vivo.deffopera.dk/individual/";
+    private static final long REQUEST_INTERVAL = 2000; // ms
+    
+    private static final Log log = LogFactory.getLog(DimensionsConnector.class);
+    private HttpUtils httpUtils = new HttpUtils();
+    private String token;
+        
+    public DimensionsConnector(String username, String password) {
+        this.token = getToken(username, password);    
+    }
+    
+    private String getToken(String username, String password) {
+        ObjectNode json = JsonNodeFactory.instance.objectNode();
+        json.put("username", username);
+        json.put("password", password);
+        String tokenJson = httpUtils.getHttpPostResponse(
+                DIMENSIONS_API + "auth.json", json.toString(), "application/json");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode tokenObj;
+        try {
+            tokenObj = mapper.readTree(tokenJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return tokenObj.get("token").textValue();
+    }
+    
+    @Override
+    protected IteratorWithSize<Model> getSourceModelIterator() {
+        return new DimensionsIterator(this.token);
+    }
+    
+    private class DimensionsIterator implements IteratorWithSize<Model> {
+
+        private static final int RESULTS_PER_REQUEST = 200;
+        private String token;
+        private final String[] sources = {
+                "publications", "grants", "patents", "clinical_trials"};
+        // number of requests to be made for each data source (pubs, grants, etc.)
+        private int[] totals = new int[4];
+        private int requestCount;
+        private Model firstIteration;
+        
+        public DimensionsIterator(String token) {
+            this.token = token;
+            for(int i = 0; i < totals.length; i++) {
+                // run a request for each data source at least once, then set
+                // the actual total results based on the number returned
+                totals[i] = 1;
+            } 
+            try {
+                firstIteration = getResults();
+                setTotals();
+            } catch (InterruptedException e) {
+                log.error(e, e);
+                throw new RuntimeException(e);
+            }
+        }
+        
+        private void setTotals() {
+            for(int i = 0; i < sources.length; i++) {
+                StmtIterator sit = firstIteration.listStatements(
+                        null, firstIteration.getProperty(
+                                XmlToRdf.GENERIC_NS + sources[i]), (RDFNode) null);
+                if(sit.hasNext()) {
+                    Statement stmt = sit.next();
+                    Resource r = stmt.getSubject();
+                    Statement stats = r.getProperty(firstIteration.getProperty(
+                            XmlToRdf.GENERIC_NS + "_stats"));
+                    if(stats != null) {
+                        RDFNode statsNode = stats.getObject();
+                        if(statsNode.isResource()) {
+                            Resource statsRes = statsNode.asResource();
+                            Statement totalCount = statsRes.getProperty(
+                                    firstIteration.getProperty(
+                                            XmlToRdf.GENERIC_NS + "total_count"));
+                            if(totalCount != null) {
+                                RDFNode totalCountNode = totalCount.getObject();
+                                if(totalCountNode.isLiteral()) {
+                                    try {
+                                        int totalCountInt = totalCountNode.asLiteral().getInt();
+                                        log.info(totalCountInt + " total " + sources[i]);
+                                        totals[i] = totalCountInt / RESULTS_PER_REQUEST + 1;
+                                    } catch (Exception e) {
+                                        log.error(e, e);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        @Override
+        public boolean hasNext() {
+            for(int i = 0; i < totals.length; i++) {
+               if(requestCount < totals[i]) {
+                   return true;
+               }
+            }
+            return false;
+        }
+
+        @Override
+        public Model next() {
+            Model results;
+            if(requestCount == 0) {                
+                results = firstIteration;
+                firstIteration = null;
+            } else {
+                try {
+                    results = getResults();
+                } catch (InterruptedException e) {
+                    log.error(e, e);
+                    throw new RuntimeException(e);
+                }
+            }
+            requestCount++;
+            return results;
+        }
+
+        @Override
+        public Integer size() {
+            int size = 0;
+            for(int i = 0; i < totals.length; i++) {
+                if(totals[i] > size) {
+                    size = totals[i];
+                }
+            }
+            return size;
+        }
+
+        @Override
+        public void close() {
+            // no API method for logging out; nothing to do for now
+        }
+        
+        private Model getResults() throws InterruptedException {
+            Model model = ModelFactory.createDefaultModel();
+            if(requestCount < totals[0]) {
+                model.add(toRdf(getPubs(this.token, requestCount * RESULTS_PER_REQUEST)));
+            }
+            if(requestCount < totals[1]) {
+                model.add(toRdf(getGrants(this.token, requestCount * RESULTS_PER_REQUEST)));
+            }
+            if(requestCount < totals[2]) {
+                model.add(toRdf(getPatents(this.token, requestCount * RESULTS_PER_REQUEST)));
+            }
+            if(requestCount < totals[3]) {
+                model.add(toRdf(getClinicalTrials(this.token, requestCount * RESULTS_PER_REQUEST)));
+            }
+            return model;
+        }
+        
+        long lastRequest = 0;
+        
+        private String getDslResponse(String dslQuery, String token) 
+                throws InterruptedException {
+            long now = System.currentTimeMillis();
+            long toWait = REQUEST_INTERVAL - (now - lastRequest);        
+            if(toWait > 0) {
+                Thread.sleep(toWait);
+            }
+            lastRequest = System.currentTimeMillis();
+            return httpUtils.getHttpPostResponse(DIMENSIONS_API + "dsl.json",
+                    dslQuery, "application/json", token);        
+        }
+        
+        private Model toRdf(String data) {                
+            try {
+                //ObjectMapper mapper = new ObjectMapper();
+                //JsonNode dataObj;
+                //dataObj = mapper.readTree(data);
+                //System.out.println(mapper.writerWithDefaultPrettyPrinter()
+                //        .writeValueAsString(dataObj));
+                JsonToXMLConverter json2xml = new JsonToXMLConverter();
+                XmlToRdf xml2rdf = new XmlToRdf();
+                RdfUtils rdfUtils = new RdfUtils();
+                String xml = json2xml.convertJsonToXml(data);
+                Model rdf = xml2rdf.toRDF(xml);
+                rdf = rdfUtils.renameBNodes(rdf, ABOX + "n", rdf);
+                rdf = renameByIdentifier(rdf, rdf.getProperty(
+                        XmlToRdf.GENERIC_NS + "id"), ABOX, "");
+                return rdf;
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        
+        private String getGrants(String token, int skip) throws InterruptedException {
+            String queryStr = "search grants where"
+                    + " (start_year >= 2014 and start_year <= 2017 and research_orgs.id = \"grid.5170.3\")"
+                    + " return grants[active_year + end_date + funders + id + project_num + start_date + start_year + title + abstract + funding_eur + grant_number + investigator_details + research_orgs]"
+                    + " limit 200 skip " + skip;
+            return getDslResponse(queryStr, token);
+        }
+        
+        private String getPubs(String token, int skip) throws InterruptedException {
+            String queryStr = "search publications where"
+                    + " (year >= 2014 and year <= 2017 and research_orgs.id = \"grid.5170.3\" and type in [\"article\", \"chapter\", \"proceeding\"])"
+                    //+ " return publications"
+                    + " return publications[id + type + title + authors + doi + pmid + pmcid + date + year + mesh_terms + journal + issn + volume + issue]"
+                    + " limit 200 skip " + skip;
+            return getDslResponse(queryStr, token);
+        }
+        
+        private String getPatents(String token, int skip) throws InterruptedException {
+            String queryStr = "search patents where"
+                    + " (granted_year >= 2014 and granted_year <= 2017 and assignees.id = \"grid.5170.3\")"
+                    + " return patents[assignees + granted_year + id + title + inventor_names + funders + abstract + associated_grant_ids]"
+                    + " limit 200 skip " + skip;
+            return getDslResponse(queryStr, token);
+        }
+        
+        private String getClinicalTrials(String token, int skip) throws InterruptedException {
+            String queryStr = "search clinical_trials where"
+                    + " (active_years >= 2014 and active_years <= 2017 and organizations.id = \"grid.5170.3\")"
+                    + " return clinical_trials[id + title + abstract + active_years + associated_grant_ids + date + researchers + organizations + funders]"
+                    + " limit 200 skip " + skip;
+            return getDslResponse(queryStr, token);
+        }
+        
+        protected Model renameByIdentifier(Model m, Property identifier, 
+                String namespace, String localNamePrefix) {
+            Map<Resource, String> idMap = new HashMap<Resource, String>();
+            StmtIterator sit = m.listStatements(null, identifier, (RDFNode) null);
+            while(sit.hasNext()) {
+                Statement stmt = sit.next();
+                if(stmt.getObject().isLiteral()) {
+                    idMap.put(stmt.getSubject(), 
+                            //stripNonWordChars(stmt.getObject().asLiteral().getLexicalForm()));
+                            stmt.getObject().asLiteral().getLexicalForm());
+                }
+            }
+            for(Resource res : idMap.keySet()) {
+                ResourceUtils.renameResource(
+                        res, namespace + localNamePrefix + idMap.get(res));
+            }
+            return m;
+        }
+        
+        protected String stripNonWordChars(String value) {
+            if(value == null) {
+                return value;
+            } else {
+                return value.replaceAll("\\W", "");
+            }
+        }
+
+        
+    }
+
+    @Override
+    protected Model filter(Model model) {
+        return model;
+    }
+
+    @Override
+    protected Model mapToVIVO(Model model) {
+        return model;
+    }
+
+    @Override
+    protected String getPrefixName() {
+        return "dimensions";
+    }
+    
+}
+
+
+

884a7fc745ec9784d6dd353e3a4c20fa48c0cae0
Brian Lowe <brian@ontocale.com>
2020-03-28 14:19:43 +0200

    Add clinical trials mappings

--- etl/src/main/java/dk/deffopera/nora/vivo/etl/datasource/connector/dimensions/DimensionsClinicalTrialsConnector.java
+++ etl/src/main/java/dk/deffopera/nora/vivo/etl/datasource/connector/dimensions/DimensionsClinicalTrialsConnector.java
@@ -1,7 +1,12 @@
 package dk.deffopera.nora.vivo.etl.datasource.connector.dimensions;
 
 import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.jena.rdf.model.Model;
@@ -39,6 +44,8 @@ public class DimensionsClinicalTrialsConnector extends DimensionsConnector {
                     .noCursorTimeout(true).iterator();
         }
         
+        private Set<String> roles = new HashSet<String>();
+        
         @Override
         public Model next() {
             Model results = ModelFactory.createDefaultModel();
@@ -64,20 +71,44 @@ public class DimensionsClinicalTrialsConnector extends DimensionsConnector {
                         for(int authi = 0; authi < authors.length(); authi++) {
                             JSONObject author = authors.getJSONObject(authi);
                             author.put("authorRank", authi + 1);
+                            if(author.has("last_name")) {
+                                author.put("last_name_normalized", StringUtils.stripAccents(
+                                        author.getString("last_name").toLowerCase()));
+                            }
                         }    
                     }
                     if(!jsonObj.has("investigator_details")) {
                         log.info("investigator details not found");
                     } else {
                         log.info("investigator details found");
+                        JSONArray investigatorsParsed = new JSONArray();
                         JSONArray investigators = jsonObj.getJSONArray("investigator_details");
                         for(int invi = 0; invi < investigators.length(); invi++) {
                             JSONArray investigator = investigators.getJSONArray(invi);
-                            int invj = 0;
-                            for(invj = 0; invj < investigator.length(); invj++) {
-                                investigator.put(invj, (invj + 1) + "|" + investigator.get(invj));
-                            }
-                            investigator.put(invj, (invj + 1) + "|rank" + (invi + 1));
+                            JSONObject investigatorParsed = new JSONObject();
+                            investigatorParsed.put("name", investigator.get(0));
+                            investigatorParsed.put("name_normalized", StringUtils.stripAccents(
+                                    investigator.getString(0).toLowerCase()));
+                            investigatorParsed.put("role", investigator.get(2));
+                            roles.add(investigator.getString(2));                        
+                            investigatorParsed.put("org_line_1", investigator.get(3));                                                
+                            investigatorParsed.put("org_line_2", investigator.get(4));                                                
+                            investigatorParsed.put("grid", investigator.get(5));                        
+                            investigatorParsed.put("authorRank", invi + 1);
+                            investigatorsParsed.put(invi, investigatorParsed);
+                        }
+                        jsonObj.put("investigators_parsed", investigatorsParsed);
+                    }
+                    if(!jsonObj.has("active_years")) { 
+                        log.info("active_years not found");
+                    } else {
+                        log.info("active_years found");
+                        JSONArray activeYears = jsonObj.getJSONArray("active_years");
+                        if(activeYears.isEmpty()) {
+                            log.warn("active_years array is empty");
+                        } else {
+                            jsonObj.put("start_year", activeYears.get(0));
+                            jsonObj.put("end_year", activeYears.get(activeYears.length() - 1));
                         }
                     }
                 } catch (JSONException e) {
@@ -89,27 +120,37 @@ public class DimensionsClinicalTrialsConnector extends DimensionsConnector {
             }
             StringWriter out = new StringWriter();
             results.write(out, "N3");
-            log.info(out.toString());
-            return results;
-        }                
+            log.info(out.toString());           
+            return results;            
+        }
         
+        @Override
+        public void close() {            
+            log.info("**** List of roles");
+            for(String role : roles) {
+                log.info(role);
+            }
+            super.close();
+        }
     }
+    
     
     @Override
     protected Model mapToVIVO(Model model) {
-//        List<String> queries = Arrays.asList("050-datasetId.rq");
-//        for(String query : queries) {
-//            construct(SPARQL_RESOURCE_DIR + "datasets/" + query, model, ABOX + getPrefixName() + "-");
-//        }        
-//        model = renameByIdentifier(model, model.getProperty("tmp:datasetId"), ABOX, "dataset.");
-//        queries = Arrays.asList(
-//                "100-dataset.rq",
-//                "140-datasetAuthorship.rq",
-//                "190-for.rq"
-//                );
-//        for(String query : queries) {
-//            construct(SPARQL_RESOURCE_DIR + "datasets/" + query, model, ABOX + getPrefixName() + "-");
-//        }
+        List<String> queries = Arrays.asList("050-clinicalTrialId.rq");
+        for(String query : queries) {
+            construct(SPARQL_RESOURCE_DIR + "clinical_trials/" + query, model, ABOX + getPrefixName() + "-");
+        }        
+        model = renameByIdentifier(model, model.getProperty("tmp:clinicalTrialId"), ABOX, "clinical_trial.");
+        queries = Arrays.asList(
+                "100-clinicalTrial.rq",                
+                "190-for.rq",
+                "200-investigator.rq",
+                "210-funder.rq"
+                );
+        for(String query : queries) {
+            construct(SPARQL_RESOURCE_DIR + "clinical_trials/" + query, model, ABOX + getPrefixName() + "-");
+        }
         return model;
     }
 

c8680f1445ec411c980d1be4c321118609ea3cde
Brian Lowe <brian@ontocale.com>
2020-03-31 14:56:13 +0300

    Fix import

--- custom-vivo/webapp/src/main/java/edu/cornell/mannlib/vitro/webapp/search/controller/PagedSearchController.java
+++ custom-vivo/webapp/src/main/java/edu/cornell/mannlib/vitro/webapp/search/controller/PagedSearchController.java
@@ -24,8 +24,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import com.hp.hpl.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.OWL;
 
 import dk.deffopera.nora.vivo.search.NoraSearchFacets;
 import dk.deffopera.nora.vivo.search.SearchFacet;

e0b59bfe55831478f5c2f3e7352f8a1c31fb1635
Brian Lowe <brian@ontocale.com>
2020-06-01 14:37:22 +0300

    Add parent facets to overall list

--- custom-vivo/webapp/src/main/java/dk/deffopera/nora/vivo/search/NoraSearchFacets.java
+++ custom-vivo/webapp/src/main/java/dk/deffopera/nora/vivo/search/NoraSearchFacets.java
@@ -1,6 +1,7 @@
 package dk.deffopera.nora.vivo.search;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,7 @@ public class NoraSearchFacets {
 
     private static List<SearchFacet> commonSearchFacets = new ArrayList<SearchFacet>();
     private static List<SearchFacet> additionalSearchFacets = new ArrayList<SearchFacet>();
+    private static List<SearchFacet> parentSearchFacets = new ArrayList<SearchFacet>();
     private static List<SearchFacetAsText> searchFacetsAsText = new ArrayList<SearchFacetAsText>();
     private static Map<String, SearchFacet> facetsByFieldName = new HashMap<String, SearchFacet>();
     private static final boolean DISPLAY_IN_SIDEBAR = true;
@@ -26,6 +28,7 @@ public class NoraSearchFacets {
         
         SearchFacet danishOrgsParent = new SearchFacet("facet_organization-all_ss", "Danish Universities & Hospitals", DISPLAY_IN_SIDEBAR, IS_UNION_FACET);
         allFacets.add(danishOrgsParent);
+        parentSearchFacets.add(danishOrgsParent);
         commonSearchFacets.add(new SearchFacet("facet_university_ss", "Danish Universities", DISPLAY_IN_SIDEBAR, IS_UNION_FACET, danishOrgsParent));
         commonSearchFacets.add(new SearchFacet("facet_hospital_ss", "Danish Hospitals", DISPLAY_IN_SIDEBAR, IS_UNION_FACET, danishOrgsParent));
         
@@ -100,11 +103,11 @@ public class NoraSearchFacets {
 
     public static List<String> getFacetFields() {
         ArrayList<String> facets = new ArrayList<String>();
-        for(SearchFacet sf : commonSearchFacets) {
-            facets.add(sf.getFieldName());
-        }
-        for(SearchFacet sf : additionalSearchFacets) {
-            facets.add(sf.getFieldName());
+        for(List<SearchFacet> facetList : Arrays.asList(commonSearchFacets, 
+                additionalSearchFacets, parentSearchFacets)) {
+            for(SearchFacet sf : facetList) {
+                facets.add(sf.getFieldName());
+            }    
         }
         return facets;
     }

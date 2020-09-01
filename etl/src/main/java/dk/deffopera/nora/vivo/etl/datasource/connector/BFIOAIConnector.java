c0ad39c1af3bb78eaa9f487eaca7483854b0f643
Brian Lowe <brian@ontocale.com>
2020-09-01 15:49:53 +0300

    Update BFIOAI connector for generalizability across different Mongo collections

--- etl/src/main/java/dk/deffopera/nora/vivo/etl/datasource/connector/BFIOAIConnector.java
+++ etl/src/main/java/dk/deffopera/nora/vivo/etl/datasource/connector/BFIOAIConnector.java
@@ -1,39 +1,45 @@
 package dk.deffopera.nora.vivo.etl.datasource.connector;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.vocabulary.RDF;
 import org.bson.Document;
 import org.json.JSONObject;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mongodb.client.MongoCollection;
 
 import dk.deffopera.nora.vivo.etl.datasource.IteratorWithSize;
 import dk.deffopera.nora.vivo.etl.datasource.connector.dimensions.DimensionsConnector;
 import dk.deffopera.nora.vivo.etl.datasource.connector.dimensions.DimensionsHealthcareOrgsConnector;
-import dk.deffopera.nora.vivo.etl.util.JsonToXMLConverter;
-import dk.deffopera.nora.vivo.etl.util.RdfUtils;
 import dk.deffopera.nora.vivo.etl.util.XmlToRdf;
 
 public class BFIOAIConnector extends DimensionsConnector {
 
     private static final Log log = LogFactory.getLog(DimensionsHealthcareOrgsConnector.class);
     
+    private String collectionName;
+    
     public BFIOAIConnector(String username, String password, String mongoServer, String mongoPort,
             String mongoCollection, String mongoUsername, String mongoPassword) {
         super(username, password, mongoServer, mongoPort, mongoCollection, 
                 mongoUsername, mongoPassword);
+        this.collectionName = mongoCollection;
     }
     
     @Override
     public int getBatchSize() {
-        return 1000;
+        return 1;
     }
     
     @Override
@@ -75,7 +81,8 @@ public class BFIOAIConnector extends DimensionsConnector {
     
     @Override
     protected Model mapToVIVO(Model model) {
-        model = renameByIdentifier(model, model.getProperty(XmlToRdf.GENERIC_NS + "DOI"), ABOX, "bfioai-doi-");
+        model = renameByIdentifier(model, model.getProperty(XmlToRdf.GENERIC_NS + "doi"), ABOX, collectionName + "-doi-");
+        model = renameByIdentifier(model, model.getProperty(XmlToRdf.GENERIC_NS + "DOI"), ABOX, collectionName + "-doi-");
         List<String> queries = Arrays.asList(
                 "100-bfioai.rq"
                 );
@@ -84,10 +91,26 @@ public class BFIOAIConnector extends DimensionsConnector {
         }
         return model;
     }
+    
+    @Override 
+    public Model filter(Model model) {
+       model = super.filter(model);
+       List<Statement> typeStmts = new ArrayList<Statement>();
+       ResIterator rit = model.listSubjects();
+       while(rit.hasNext()) {
+           Resource r = rit.next();
+           typeStmts.add(ResourceFactory.createStatement(
+                   r, RDF.type, model.getResource(
+                           "http://vivo.deffopera.dk/ontology/osrap/" 
+                                   + collectionName.toUpperCase())));
+       }
+       model.add(typeStmts);
+       return model;
+    }
 
     @Override
     protected String getPrefixName() {
-        return "dimensions-healthcare_orgs";
+        return "bfioai";
     }
 
 }

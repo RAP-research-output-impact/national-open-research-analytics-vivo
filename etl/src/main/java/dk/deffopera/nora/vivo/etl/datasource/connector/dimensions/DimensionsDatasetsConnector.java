53484001bbd33bc158f471f05f7f90edd717007e
Brian Lowe <brian@ontocale.com>
2020-11-11 13:13:50 +0200

    Remove long-running cursor from datasets connector

--- etl/src/main/java/dk/deffopera/nora/vivo/etl/datasource/connector/dimensions/DimensionsDatasetsConnector.java
+++ etl/src/main/java/dk/deffopera/nora/vivo/etl/datasource/connector/dimensions/DimensionsDatasetsConnector.java
@@ -1,6 +1,8 @@
 package dk.deffopera.nora.vivo.etl.datasource.connector.dimensions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -8,11 +10,13 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
 
 import dk.deffopera.nora.vivo.etl.datasource.IteratorWithSize;
@@ -34,49 +38,88 @@ public class DimensionsDatasetsConnector extends DimensionsConnector {
     
     protected class GrantsIterator extends MongoIterator implements IteratorWithSize<Model> {
 
+        private List<String> defaultids = new ArrayList<String>();
+        private Iterator<String> defaultidIt;        
+        private int iteration = 0;
+        
         public GrantsIterator(MongoCollection<Document> collection) {
             this.collection = collection;
-            this.cursor = collection.find(Filters.eq("meta.raw.dbname", "datasets"))
-                    .noCursorTimeout(true).iterator();
+            try {
+                this.cursor = collection.find(Filters.eq("meta.raw.dbname", "datasets"))
+                        .projection(new Document("meta.defaultid", 1))
+                        .noCursorTimeout(true).iterator();
+                while(cursor.hasNext()) {
+                    String jsonStr = cursor.next().toJson();
+                    JSONObject json = new JSONObject(jsonStr);
+                    String defaultid = json.getJSONObject("meta").getString("defaultid");
+                    defaultids.add(defaultid);
+                }
+            } finally {
+                if(this.cursor != null) {
+                    cursor.close();
+                }
+            }
+            this.defaultidIt = defaultids.iterator();
+            log.info(defaultids.size() + " documents to retrieve");
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return defaultidIt.hasNext();
         }
         
         @Override
         public Model next() {
+            iteration++;
             Model results = ModelFactory.createDefaultModel();
+            long start = System.currentTimeMillis();
             int batch = MONGO_DOCS_PER_ITERATION;
-            while(batch > 0 && cursor.hasNext()) {
+            List<Bson> filters = new ArrayList<Bson>();
+            while(batch > 0 && defaultidIt.hasNext()) {
+                //while(batch > 0 && cursor.hasNext()) {
                 batch--;
-                long start = System.currentTimeMillis();
-                log.info("Getting next document from cursor");
-                Document d = cursor.next();
-                log.info((System.currentTimeMillis() - start) + " ms to retrieve document");
-                String jsonStr = d.toJson();   
-                JSONObject fullJsonObj = new JSONObject(jsonStr);
-                log.info(fullJsonObj.toString(2));                
-                JSONObject jsonObj = fullJsonObj.getJSONObject("meta").getJSONObject("raw");
-                if(log.isDebugEnabled()) {
-                    log.debug(jsonObj.toString(2));
-                }                
-                try {                  
-                    if(!jsonObj.has("researchers")) {
-                        log.info("researchers not found");
-                    } else {
-                        log.info("researchers found");
-                        JSONArray authors = jsonObj.getJSONArray("researchers");
-                        for(int authi = 0; authi < authors.length(); authi++) {
-                            JSONObject author = authors.getJSONObject(authi);
-                            author.put("authorRank", authi + 1);
-                        }    
-                    }                                                        
-                } catch (JSONException e) {
-                    log.info(jsonObj.toString(2));
-                    throw (e);
-                }
-                results.add(toRdf(fullJsonObj.toString()));
+                String defaultid = defaultidIt.next();
+                filters.add(Filters.eq("meta.defaultid", defaultid));
             }
-            return results;
+            log.info("RDFizing next batch of documents from MongoDB");
+            MongoCursor<Document> dcur = collection.find(Filters.or(filters)).iterator();
+            try {
+                while(dcur.hasNext()) {
+                    Document d = dcur.next();
+                    String jsonStr = d.toJson();   
+                    JSONObject fullJsonObj = new JSONObject(jsonStr);
+                    if(iteration < 2) {
+                        log.info(fullJsonObj.toString(2));
+                    }
+                    JSONObject jsonObj = fullJsonObj.getJSONObject("meta").getJSONObject("raw");
+                    if(log.isDebugEnabled()) {
+                        log.debug(jsonObj.toString(2));
+                    }                
+                    try {                  
+                        if(!jsonObj.has("researchers")) {
+                            log.debug("researchers not found");
+                        } else {
+                            log.debug("researchers found");
+                            JSONArray authors = jsonObj.getJSONArray("researchers");
+                            for(int authi = 0; authi < authors.length(); authi++) {
+                                JSONObject author = authors.getJSONObject(authi);
+                                author.put("authorRank", authi + 1);
+                            }    
+                        }                                                        
+                    } catch (JSONException e) {
+                        log.info(jsonObj.toString(2));
+                        throw (e);
+                    }
+                    results.add(toRdf(fullJsonObj.toString()));
+                }
+                log.info((System.currentTimeMillis() - start) + " ms to RDFize batch of documents");
+                return results;
+            } finally {
+                if(dcur != null) {
+                    dcur.close();
+                }
+            }
         }                
-        
     }
     
     @Override
